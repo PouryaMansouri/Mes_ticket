afe9eb42cad73885e23fded58f49ed1e9fe7eca3
kiarashfz <kiawfz3673@gmail.com>
2022-04-04 08:11:19 +0430

    SiteSettingAdmin wrote and registered

--- ticket_sales/site_settings/admin.py
+++ ticket_sales/site_settings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.admin import BaseAdmin
+from site_settings.models import SiteSetting
+
+
+class SiteSettingAdmin(BaseAdmin):
+    list_display = ['site_name', 'contact_phone', 'contact_email']
+
+
+admin.site.register(SiteSetting, SiteSettingAdmin)
