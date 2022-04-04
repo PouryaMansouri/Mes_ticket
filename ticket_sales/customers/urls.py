4d4f8e830ccf46277ab8f39fea077fc5377cf2e7
kiarashfz <kiawfz3673@gmail.com>
2022-04-04 23:05:14 +0430

    login, logout, otpapiview added to urls

--- ticket_sales/customers/urls.py
+++ ticket_sales/customers/urls.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from customers.views import UserRegister, Login, VerifyPhoneAndCreateCustomer
+from customers.views import UserRegister, VerifyPhoneAndCreateCustomer, MyLoginView
+from customers.apis import OTPAPIView
 
 app_name = 'customers'
 urlpatterns = [
     path('register', UserRegister.as_view(), name='register'),
-    path('login', Login.as_view(), name='login'),
+    path('login', MyLoginView.as_view(), name='login'),
     path('verify-phone', VerifyPhoneAndCreateCustomer.as_view(), name='verify-phone'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('otpapiview/', OTPAPIView.as_view(), name='otpapiview'),
 ]
