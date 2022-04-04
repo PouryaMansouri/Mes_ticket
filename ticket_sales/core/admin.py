d0155dbd32c8de3d69aaa366d5071a27eefe7428
kiarashfz <kiawfz3673@gmail.com>
2022-04-04 08:04:47 +0430

    UserAdmin customized by custom User model

--- ticket_sales/core/admin.py
+++ ticket_sales/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from core.models import User
+
+UserAdmin.list_display = ('phone', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('phone', 'first_name', 'last_name')
+UserAdmin.ordering = ('phone',)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+admin.site.register(User, UserAdmin)
