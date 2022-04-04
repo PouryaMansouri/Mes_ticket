d07a0239d80961250112824f31e31d646005edeb
kiarashfz <kiawfz3673@gmail.com>
2022-04-04 08:31:09 +0430

    Customer model wrote

--- ticket_sales/customers/models.py
+++ ticket_sales/customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel, User
+from django.utils.translation import gettext_lazy as _
+
+
+class Customer(BaseModel):
+    class Meta:
+        verbose_name = _('Customer')
+        verbose_name_plural = _('Customers')
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
