96a3b5a289faae803286af27f253bbe453d8337c
kiarashfz <kiawfz3673@gmail.com>
2022-04-06 02:48:59 +0430

    event model registered in admin with JFieldListFilter

--- ticket_sales/events/admin.py
+++ ticket_sales/events/admin.py
@@ -1,3 +1,17 @@
+from core.admin import BaseAdmin
+from events.models import Event
 from django.contrib import admin
 
-# Register your models here.
+from django_jalali.admin.filters import JDateFieldListFilter
+
+# you need import this for adding jalali calander widget
+import django_jalali.admin as jadmin
+
+
+class EventAdmin(BaseAdmin):
+    list_filter = (
+        ('datetime', JDateFieldListFilter),
+    )
+
+
+admin.site.register(Event, EventAdmin)
