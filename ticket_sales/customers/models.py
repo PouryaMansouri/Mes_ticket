ae93d7016d017ce2ac3421ba5040e599b50e8103
kiarashfz <kiawfz3673@gmail.com>
2022-04-06 01:34:32 +0430

    verbose names changed to persian language

--- ticket_sales/customers/models.py
+++ ticket_sales/customers/models.py
@@ -6,10 +6,11 @@ from django.utils.translation import gettext_lazy as _
 
 class Customer(BaseModel):
     class Meta:
-        verbose_name = _('Customer')
-        verbose_name_plural = _('Customers')
+        verbose_name = _('مشتری')
+        verbose_name_plural = _('مشتری')
 
     user = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name=_('کاربر')
     )
