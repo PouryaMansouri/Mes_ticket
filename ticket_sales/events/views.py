cc029b7afb2ed0f61a7b7f4cc0a678d34bb1b43d
kiarashfz <kiawfz3673@gmail.com>
2022-04-06 05:12:31 +0430

    4 events set to index template

--- ticket_sales/events/views.py
+++ ticket_sales/events/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from events.models import Event
+
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+        context['events'] = Event.objects.order_by('-datetime').all()[:4]
+        return context
