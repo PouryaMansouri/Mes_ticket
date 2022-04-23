af1a799330e05f89ca44ef52e1739b0d44b81977
kiarashfz <kiawfz3673@gmail.com>
2022-04-24 03:40:45 +0430

    file deleted

--- ticket_sales/events/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from events.models import Ticket
-
-
-@receiver(post_save, sender=Ticket)
-def my_handler(sender, **kwargs):
-    print(sender)
-    print('sender'*100)
