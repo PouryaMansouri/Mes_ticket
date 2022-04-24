b2a7b9dd12d21f27d3157d335c4713b36516f6e8
kiarashfz <kiawfz3673@gmail.com>
2022-04-24 07:32:14 +0430

    celery configs added

--- ticket_sales/ticket_sales/settings.py
+++ ticket_sales/ticket_sales/settings.py
@@ -196,3 +196,18 @@ LOGGING = {
         }
     },
 }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Celery Configuration Options
+CELERY_TIMEZONE = "Asia/Tehran"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+# todo: change redis server
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_TRANSPORT = 'redis'
+# todo: change redis server
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
