f2ac1ba0b1865f20573bbd24aa3de4fb67294234
mahdikh <mahdikhaloei5109@gmail.com>
2022-05-11 22:26:05 +0430

    add model admin to account app

--- ticket_sales/core/admin.py
+++ ticket_sales/core/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from accounts.models import User
-
-UserAdmin.list_display = ('phone_number', 'first_name', 'last_name', 'is_staff')
-UserAdmin.search_fields = ('phone_number', 'first_name', 'last_name')
-UserAdmin.ordering = ('phone_number',)
-UserAdmin.fieldsets[0][1]['fields'] = ('phone_number', 'password')
-admin.site.register(User, UserAdmin)
 
 
 class BaseAdmin(admin.ModelAdmin):
