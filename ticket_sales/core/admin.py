3d6917e13bb20f9c1007ab0b77d7e9deccaded25
mahdikh <mahdikhaloei5109@gmail.com>
2022-05-11 14:07:32 +0430

    add account app and customize User model

--- ticket_sales/core/admin.py
+++ ticket_sales/core/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from core.models import User
+from accounts.models import User
 
-UserAdmin.list_display = ('phone', 'first_name', 'last_name', 'is_staff')
-UserAdmin.search_fields = ('phone', 'first_name', 'last_name')
-UserAdmin.ordering = ('phone',)
-UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+UserAdmin.list_display = ('phone_number', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('phone_number', 'first_name', 'last_name')
+UserAdmin.ordering = ('phone_number',)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone_number', 'password')
 admin.site.register(User, UserAdmin)
 
 
--- ticket_sales/core/models.py
+++ ticket_sales/core/models.py
@@ -2,56 +2,47 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
-
-from core.manager import MyUserManager
 from events import validators
 
 
 class BaseModel(models.Model):
+    """
+        BaseModel class for all models
+
+        Attributes:
+            created_at (models.DateTimeField): Date and time of creation
+            updated_at (models.DateTimeField): Date and time of last update
+            is_deleted (models.BooleanField): True if the object is deleted
+
+
+        methods:
+           logical_delete(self): logical delete of the object
+           logical_restore(self): logical restore of the object
+
+
+        """
+
+    # Date and time of creation of the object in the database (auto_now_add=True)
+    # Date and time of last update of the object in the database (auto_now=True)
+    # True if the object is deleted (is_deleted=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
+    update_date = models.DateTimeField(auto_now=True, verbose_name=_('Update at'))
+    is_deleted = models.BooleanField(default=False, verbose_name=_('Is deleted'))
+
     class Meta:
         abstract = True
+        ordering = ('-created_at',)
 
-    created = models.DateTimeField(
-        auto_now_add=True,
-        editable=False,
-        verbose_name=_('زمان ساخت'),
-    )
+    def logical_delete(self) -> None:
+        """ logical delete of the object """
+        self.is_deleted = True
+        self.save()
 
-    last_updated = models.DateTimeField(
-        auto_now=True,
-        editable=False,
-        verbose_name=_('زمان بروزرسانی'),
-    )
-
-
-class User(AbstractUser):
-    objects = MyUserManager()
-
-    phone = models.CharField(
-        max_length=14,
-        verbose_name=_('شماره موبایل'),
-        unique=True,
-        validators=[RegexValidator(
-            regex='^(0)?9\d{9}$',
-            message=_('لطفا شماره موبایل معتبری وارد نمایید!')
-        )]
-    )
-
-    national_code = models.CharField(
-        max_length=10,
-        verbose_name=_('کدملی'),
-        unique=True,
-        validators=[validators.validate_iran_national_code]
-    )
-
-    USERNAME_FIELD = 'phone'
-
-    # TODO: add REQUIRED_FIELDS
-    # TODO: add __str__ method
-
-    def save(self, *args, **kwargs):
-        self.username = self.phone
-        super().save(*args, **kwargs)
+    def logical_restore(self) -> None:
+        """ logical restore of the object """
+        self.is_deleted = False
+        self.save()
 
 
 class SingletonBaseModel(models.Model):
