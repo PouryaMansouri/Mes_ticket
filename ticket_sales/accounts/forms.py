c082e66354c71d2d3109a59380eab1440b320ecc
mahdikh <mahdikhaloei5109@gmail.com>
2022-05-14 13:22:23 +0430

    fixed bugs

--- ticket_sales/accounts/forms.py
+++ ticket_sales/accounts/forms.py
@@ -3,37 +3,48 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
-class RegistrationForm(forms.Form):
-    phone_number = forms.CharField(label='شماره تلفن',
-                                   widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    national_code = forms.IntegerField(label='کد ملی', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label='رمز عبور',
-                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='تکرار رمز عبور', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(label='نام',
-                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(label='نام خانوادگی',
-                                widget=forms.TextInput(attrs={'class': 'form-control'}))
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['phone_number',
+                  'email',
+                  'password',
+                  'national_code',
+                  'first_name',
+                  'last_name']
+        widgets = {
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'national_code': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'phone_number': 'شماره تلفن',
+            'email': 'ایمیل',
+            'password': 'رمز عبور',
+            'national_code': 'کد ملی',
+            'first_name': 'نام',
+            'last_name': 'نام خانوادگی',
+        }
 
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        user = User.objects.filter(email=email).exists()
-        if user:
-            raise ValidationError('این ایمیل وجود دارد')
-        return email
-    #
-    # def clean(self):
-    #     p1 = self.password1
-    #     p2 = self.password2
-    #
-    #     if p1 and p2 and p1 != p2:
-    #         raise ValidationError('پسورد وارد شده همخوانی ندارد')
-    #     return p1
+        def clean_email(self):
+            email = self.cleaned_data['email']
+            user = User.objects.filter(email=email).exists()
+            if user:
+                raise ValidationError('این ایمیل وجود دارد')
+            return email
 
 
 class LoginForm(forms.Form):
     phone_number = forms.CharField(label='شماره تلفن',
                                    widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label='رمز عبور',
-                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    # class Meta:
+    #     widgets = {
+    #         'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+    #         'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+    #     }
