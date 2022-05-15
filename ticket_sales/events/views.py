4ceca03cbd348c5c80be6174ca39164c1a238c9e
mahdikh <mahdikhaloei5109@gmail.com>
2022-05-15 14:25:10 +0430

    create player model and dynamic templates

--- ticket_sales/events/views.py
+++ ticket_sales/events/views.py
@@ -3,6 +3,7 @@ from django.views.generic import FormView
 
 from events.forms import TicketForm
 from events.models import Event
+from player.models import Player
 
 
 class Index(FormView):
@@ -12,7 +13,9 @@ class Index(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        players = Player.objects.filter(goal__gte=3)
         event = Event.objects.last()
+        context['players'] = players
         context['event'] = event
         context['event_date'] = event.str_event_time
         return context
