0dcc1d638a8623582fe47d212afcb7459a6948f7
mahdikh <mahdikhaloei5109@gmail.com>
2022-05-17 14:20:26 +0430

    fixed directory

---
+++ events/admin.py
@@ -0,0 +1,28 @@
+from core.admin import BaseAdmin
+from events.models import Event, Team, Ticket
+from django.contrib import admin
+
+from django_jalali.admin.filters import JDateFieldListFilter
+
+# you need import this for adding jalali calander widget
+import django_jalali.admin as jadmin
+
+
+class EventAdmin(BaseAdmin):
+    list_filter = (
+        ('event_time', JDateFieldListFilter),
+    )
+
+
+class TicketAdmin(BaseAdmin):
+    list_display = ['event', 'full_name', 'phone', 'national_code', 'is_used']
+
+    search_fields = ['full_name', 'phone', 'national_code']
+
+    list_editable = ['is_used']
+
+    list_filter = ['is_used']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Ticket, TicketAdmin)
